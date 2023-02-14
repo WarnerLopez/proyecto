4b42f255400bb064758a962d823c4a850a85f153
Warner <lopezwarner0@gmail.com>
2023-02-13 18:01:42 -0600

    por molestar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <div class="jumbotron jumbotron-fluid mi-jumbotron">
         <div class="container">
-          <h1>VLA </h1>
+          <h1>MI amor rica </h1>
           <p>Las empresas buscan personas con habilidades informáticas, gerenciales, de idiomas y mercadeo digital. Aprende con las clases 100% en vivo por Internet de VLA. Tu rumbo a un mejor empleo empieza aquí.</p>
         </div>
       </div>
